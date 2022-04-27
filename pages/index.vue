d7a35e057a3a5208220568f7288ce60a2f75ba12
Emre Simsek <101721893+emresimsekm@users.noreply.github.com>
2022-04-27 17:31:00 +0300

    ADD: init project

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <BaNavBar />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  components: {},
+})
+</script>
